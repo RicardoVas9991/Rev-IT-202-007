f3ab434bd642f7c08e599a31a8a9b1615c1cd875
RicardoVas9991 <rvasquez9999@hotmail.com>
2024-11-30 21:15:18 -0500

    db error

--- lib/db.php
+++ lib/db.php
@@ -17,11 +17,9 @@ function getDB(){
             $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
             //using the PDO connector create a new connect to the DB
             //if no error occurs we're connected
-            $db = new PDO($connection_string, $dbuser, $dbpass);
             $db = new PDO($dsn, $username, $password);
 	    //the default fetch mode is FETCH_BOTH which returns the data as both an indexed array and associative array
 	    //we'll override the default here so it's always fetched as an associative array
- 	    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
    	catch(Exception $e){
